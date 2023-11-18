507576bb2f4f72360e3ff13a742fe3d67b73d05c
ken kivuti <kivutiken4@gmail.com>
2023-11-18 12:06:40 +0300

    not complete

--- dbservices.py
+++ dbservices.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
+from flask_login import UserMixin , LoginManager
  
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 app.config["SQLALCHEMY_DATABASE_URI"]= "postgresql://postgres:kenkivuti254@localhost:5432/mackaysdb"
 db = SQLAlchemy(app)
 
 
-class Users(db.Model):
+class Users( UserMixin , db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer , primary_key = True)
     name= db.Column(db.String(255), nullable = False)
-    phone = db.Column(db.String)
+    contact = db.Column(db.String)
     email=db.Column(db.String(255),nullable = False)
-    role=db.Column(db.String(255),nullable=False)
+    password=db.Column(db.String(255),nullable=False)
+    dashboard_content = db.Column(db.String(255), default="Default Dashboard Content")
 
 
 class Tenants(db.Model):
@@ -23,16 +26,16 @@ class Tenants(db.Model):
     full_name = db.Column(db.String(255))
     email=db.Column(db.String(255))
     phone = db.Column(db.String)
-    tenants= db.relationship("TenantHouses" , back_populates = "tenants")
+    tenanthouses= db.relationship("TenantHouses" , back_populates = "tenant")
 
 
 class Houses(db.Model):
-    __tablename__ ='houses'
+    __tablename__ = 'houses'
     id = db.Column(db.Integer , primary_key = True)
     house_number = db.Column(db.Integer , nullable = False)
     no_of_rooms = db.Column(db.Integer , nullable = False)
     rent = db.Column(db.String)
-    houses= db.relationship("TenantHouses" , back_populates = "houses")
+    tenanthouse= db.relationship("TenantHouses" , back_populates = "house")
 
 
 class TenantHouses(db.Model):
@@ -42,10 +45,11 @@ class TenantHouses(db.Model):
     house_id = db.Column(db.Integer , db.ForeignKey('houses.id'))
     start_date = db.Column(DateTime )
     end_date = db.Column(db.String)
-    tenant_houses = db.relationship("TenantHouseBills" , back_populates = 'tenant_houses')
+    tenant = db.relationship("Tenants" , back_populates = "tenanthouses")
+    house=db.relationship("Houses" , back_populates ="tenanthouse")
+    tenant_house=db.relationship("Tenanthousebills" , back_populates="tenant_houses")
 
-
-class TenantHouseBills(db.Model):
+class Tenanthousebills(db.Model):
     __tablename__ = 'tenant_house_bills'
     id = db.Column(db.Integer , primary_key = True)
     tenant_house_id = db.Column(db.Integer, db.ForeignKey('tenant_houses.id'), nullable =False)
@@ -53,7 +57,9 @@ class TenantHouseBills(db.Model):
     due_date=db.Column(db.String)
     amount= db.Column(db.Numeric)
     payment_status = db.Column(db.String)
-    payment_date=db.column(DateTime )
+    tenant_houses = db.relationship("TenantHouses" , back_populates = "tenant_house")
+    payment_date=db.Column(DateTime )
+    payments = db.relationship("Payment" , back_populates = "tenantbill")
 
 
 class Payment(db.Model):
@@ -62,7 +68,7 @@ class Payment(db.Model):
     tenant_house_bill_id = db.Column(db.Integer , db.ForeignKey('tenant_house_bills.id'), nullable =False)
     payment_method= db.Column(db.String(255))
     amount_paid = db.Column(db.Numeric)
-    tenant= db.relationship("TenantHouseBills",back_populates= "payments")
+    tenantbill= db.relationship("Tenanthousebills",back_populates= "payments")
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import render_template, request , flash 
+from flask import render_template, request , flash , redirect , url_for 
 from dbservices import *
+from flask_login import LoginManager,  login_user, current_user ,login_manager,login_required
+login_manager = LoginManager(app)
 
 with app.app_context():
     db.create_all()
@@ -13,32 +15,96 @@ def mackays():
 
 
 @app.route("/households")
-def households():
+def houses():
     records = Houses.query.all()
     households=[house for house in records]
     return render_template("households.html", households=households)
 
 
 
-@app.route("/register")
+@app.route("/register" ,methods=['POST', 'GET'])
 def register():
+    if request.method=='POST':
+       name=request.form["name"]
+       contact=request.form["contact"]
+       email=request.form["email"]
+       password=request.form['password']
+
+     #    check if email exist
+       user=Users.query.filter_by(email=email).first()
+
+       if user:
+         flash("email already exist")
+       else:
+        new_user=Users(name=name ,contact=contact, email=email , password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registered successfully")
+        return redirect(url_for('login'))
     return render_template("register.html")
 
 
-@app.route("/login")
-def login():
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
-  return render_template("login.html")
-    
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = Users.query.filter_by(email=email, password=password).first()
+
+        if user:
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid email or password', 'error')
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    flash(f'Welcome to the dashboard, {current_user.email}! Content: {current_user.dashboard_content}', 'info')
+    return render_template('dashboard.html')
+
+
+@app.route("/tenants" , methods=['POST' , 'GET'])
+def tenant():
+   if request.method == 'POST' :
+      full_name=request.form['full_name']
+      email=request.form['email']
+      phone=request.form['phone']
+
+      # add new tenants
+      new_tenant=Tenants(full_name=full_name,email=email,phone=phone)
+   
+      # add the new tenant to the database
+
+      db.session.add(new_tenant)
+
+    #   commit changes to the database
+      db.session.commit()
+      flash("tenant added succesfully")
+      
+   
+   records = Tenants.query.all()
+   tenants = [tnt for tnt in records]
+   return render_template("tenants.html", tenants=tenants)
+  
+
+  
+   
+   
+
+
 
 if __name__== "__main__":
-    app.run(debug=True)
+ app.run(debug=True)  
+    
 
-    # if user:
-        #     flash("Email already exist")
-        # else:
-        #     new_user = Users(email=email, password=hashed_pass)
-        #     db.session.add(new_user)
-        #     db.session.commit()
-        #     flash("You have succesfully registered")
-        #     return redirect(url_for('login'))
+  
